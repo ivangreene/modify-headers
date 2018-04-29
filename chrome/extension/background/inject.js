cb6b41e634b5dca4d3e31a2633b9265d8c4164be
Ivan Greene <ivan@ivan.sh>
2018-04-29 15:26:21 -0500

    working but messy injection

--- chrome/extension/background/inject.js
+++ chrome/extension/background/inject.js
@@ -1,83 +1,80 @@
-let sites = [];
+(function () {
+  let sites = [],
+      headers = [],
+      rules = [];
 
-chrome.storage.sync.get('state', (obj) => {
-  const { state } = obj;
-  const initialState = JSON.parse(state || '{ "sites": [] }');
-  sites = initialState.sites.map(site => ({ ...site,
-    pattern: RegExp(site.pattern) }));
-});
-
-chrome.runtime.onMessage.addListener(function (data) {
-  if (data.type === "statechange") {
-    sites = data.state.sites.map(site => ({ ...site,
+  chrome.storage.sync.get('state', (obj) => {
+    const state = JSON.parse(obj.state || `{
+      "sites": [], "headers": [], "rules": [],
+    }`);
+    sites = state.sites.map(site => ({ ...site,
       pattern: RegExp(site.pattern) }));
-  }
-});
+    headers = state.headers;
+    rules = state.rules.map(rule => ({ ...rule, match: RegExp(rule.match) }));
+  });
 
-chrome.webRequest.onHeadersReceived.addListener(function (request) {
-  for (let i = 0; i < sites.length; i++) {
-    if (sites[i].pattern.test(request.url)) {
-      for (let j = 0; j < request.responseHeaders.length; j++) {
-        for (let k = 0; k < sites[i].headers.length; k++) {
-          if (request.responseHeaders[j].name === sites[i].headers[k].header) {
-            for (let m = 0; m < sites[i].headers[k].rules.length; m++) {
-              request.responseHeaders[j].value =
-                request.responseHeaders[j].value.replace(
-                  RegExp(sites[i].headers[k].rules[m].match),
-                  sites[i].headers[k].rules[m].replacement);
+  chrome.runtime.onMessage.addListener(function (data) {
+    if (data.type === "statechange") {
+      sites = data.state.sites.map(site => ({ ...site,
+        pattern: RegExp(site.pattern) }));
+      headers = data.state.headers;
+      rules = data.state.rules.map(rule => ({
+        ...rule, match: RegExp(rule.match)
+      }));
+    }
+  });
+
+  chrome.webRequest.onHeadersReceived.addListener(function (request) {
+
+    let count = 0;
+    debugger;
+    sites.forEach(site => {
+      if (site.pattern.test(request.url)) {
+        headers.filter(header => header.siteId === site.id)
+          .forEach(header => {
+            for (let i = 0; i < request.responseHeaders.length; i++) {
+              if (request.responseHeaders[i].name === header.header) {
+                count++;
+                rules.filter(rule => rule.headerId === header.id)
+                  .forEach(rule => {
+                    request.responseHeaders[i].value =
+                      request.responseHeaders[i].value.replace(
+                        rule.match, rule.replacement);
+                  });
+              }
+            }
+          });
+      }
+    });
+/*
+    for (let i = 0; i < sites.length; i++) {
+      if (sites[i].pattern.test(request.url)) {
+        for (let j = 0; j < request.responseHeaders.length; j++) {
+          for (let k = 0; k < sites[i].headers.length; k++) {
+            if (request.responseHeaders[j].name === sites[i].headers[k].header) {
+              for (let m = 0; m < sites[i].headers[k].rules.length; m++) {
+                request.responseHeaders[j].value =
+                  request.responseHeaders[j].value.replace(
+                    RegExp(sites[i].headers[k].rules[m].match),
+                    sites[i].headers[k].rules[m].replacement);
+              }
             }
           }
         }
       }
     }
-  }
-  console.dir(sites);
-  console.dir(request.responseHeaders);
-  return { responseHeaders: request.responseHeaders };
-}, {
-  urls: ["<all_urls>"],
-  types: ["main_frame", "sub_frame"],
-}, ["blocking", "responseHeaders"]);
-
-function isInjected(tabId) {
-  return chrome.tabs.executeScriptAsync(tabId, {
-    code: `var injected = window.reactExampleInjected;
-      window.reactExampleInjected = true;
-      injected;`,
-    runAt: 'document_start'
-  });
-}
-
-function loadScript(name, tabId, cb) {
-  if (process.env.NODE_ENV === 'production') {
-    chrome.tabs.executeScript(tabId, { file: `/js/${name}.bundle.js`, runAt: 'document_end' }, cb);
-  } else {
-    // dev: async fetch bundle
-    fetch(`http://localhost:3000/js/${name}.bundle.js`)
-    .then(res => res.text())
-    .then((fetchRes) => {
-      // Load redux-devtools-extension inject bundle,
-      // because inject script and page is in a different context
-      const request = new XMLHttpRequest();
-      request.open('GET', 'chrome-extension://lmhkpmbekcpmknklioeibfkpmmfibljd/js/redux-devtools-extension.js');  // sync
-      request.send();
-      request.onload = () => {
-        if (request.readyState === XMLHttpRequest.DONE && request.status === 200) {
-          chrome.tabs.executeScript(tabId, { code: request.responseText, runAt: 'document_start' });
-        }
-      };
-      chrome.tabs.executeScript(tabId, { code: fetchRes, runAt: 'document_end' }, cb);
-    });
-  }
-}
-
-const arrowURLs = ['^https://github\\.com'];
-
-chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
-  if (changeInfo.status !== 'loading' || !tab.url.match(arrowURLs.join('|'))) return;
-
-  const result = await isInjected(tabId);
-  if (chrome.runtime.lastError || result[0]) return;
-
-  loadScript('inject', tabId, () => console.log('load inject bundle success!'));
-});
+*/
+    if (false && request.tabId !== -1 && count > 0) {
+      debugger;
+      chrome.browserAction.setBadgeText({
+        text: `${count}`, tabId: request.tabId
+      });
+    }
+//  console.dir(sites);
+//  console.dir(request.responseHeaders);
+    return { responseHeaders: request.responseHeaders };
+  }, {
+    urls: ["<all_urls>"],
+    types: ["main_frame", "sub_frame"],
+  }, ["blocking", "responseHeaders"]);
+}());
